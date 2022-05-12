51bfb5ccca772ae349fc9a52e47194fa09b784e8
John-K408 <jkoomson23@amherst.edu>
2022-05-12 12:32:47 +0200

    -Created "Group" object and coded "groupsAdapter" template

---
+++ app/src/main/java/hu/ait/shoppingcart/adapter/groupsAdapter.kt
@@ -0,0 +1,32 @@
+package hu.ait.shoppingcart.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class groupsAdapter: RecyclerView.Adapter<groupsAdapter.ViewHolder> {
+
+    constructor(){
+
+    }
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+    }
+}
---
+++ app/src/main/java/hu/ait/shoppingcart/data/Group.kt
@@ -0,0 +1,3 @@
+package hu.ait.shoppingcart.data
+
+data class Group(var groupId:String, var groupName: String, var isHost:Boolean)
