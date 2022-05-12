19fac9426302a3cdbc60f89a5042659f514f763f
JosephTadrous <joseph.nabil07@gmail.com>
2022-05-12 15:47:01 +0200

    finished implementing login activity and user auth

--- app/src/main/java/hu/ait/shoppingcart/LoginActivity.kt
+++ app/src/main/java/hu/ait/shoppingcart/LoginActivity.kt
@@ -1,14 +1,74 @@
 package hu.ait.shoppingcart
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import hu.ait.shoppingcart.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityLoginBinding
+    lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    fun loginClick(v: View) {
+        if (!isFormValid()){
+            return
+        }
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(
+            binding.etEmail.text.toString(), binding.etPassword.text.toString()
+        ).addOnSuccessListener {
+            Toast.makeText(this@LoginActivity,
+                "Login OK",
+                Toast.LENGTH_LONG).show()
+
+            // navigate to other Activity
+            startActivity(Intent(this, GroupsActivity::class.java))
+
+        }.addOnFailureListener{
+            Toast.makeText(this@LoginActivity,
+                "Error: ${it.message}",
+                Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+    fun registerClick(v: View){
+        if (!isFormValid()){
+            return
+        }
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+            binding.etEmail.text.toString(), binding.etPassword.text.toString()
+        ).addOnSuccessListener {
+            Toast.makeText(this@LoginActivity,
+                "Registration OK",
+                Toast.LENGTH_LONG).show()
+        }.addOnFailureListener{
+            Toast.makeText(this@LoginActivity,
+                "Error: ${it.message}",
+                Toast.LENGTH_LONG).show()
+        }
+    }
+
+    fun isFormValid(): Boolean {
+        return when {
+            binding.etEmail.text.isEmpty() -> {
+                binding.etEmail.error = "This field can not be empty"
+                false
+            }
+            binding.etPassword.text.isEmpty() -> {
+                binding.etPassword.error = "The password can not be empty"
+                false
+            }
+            else -> true
+        }
     }
 }
